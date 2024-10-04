a8b4bbfc633094ace09de1799924489d194a789d
godjihye <njih112@gmail.com>
2024-10-04 15:19:13 +0900

    1004 수정 기능 추가, row number

--- app.js
+++ app.js
@@ -64,19 +64,41 @@ app.get("/", (req, res) => {
   if (req.session.user) {
     const userData = req.session.user;
     const userId = userData["id"];
-    console.log(userId);
-    let sql = `
-      SELECT h.id as id, h.habit_name as habit_name, h.start_date as start_date, h.end_date as end_date, COUNT(r.habit_id) AS record_count FROM habits h LEFT JOIN records r ON h.id = r.habit_id WHERE h.user_id = ${userId} GROUP BY h.id, h.habit_name,h.start_date, h.end_date ORDER BY h.id desc
-    `;
-    let sql2 = `select id, habit_name, start_date, end_date, (select count(1) from records r where r.habit_id=h.id) count from habits h where user_id=${userId} ORDER BY h.id desc`;
 
-    db.all(sql2, [], (err, rows) => {
+    let page = req.query.page ? parseInt(req.query.page) : 1;
+    const limit = 5;
+    const offset = (page - 1) * limit;
+    let sql = `select row_number() over(order by id) as rn, id, habit_name, start_date, end_date, (select count(1) from records r where r.habit_id = h.id) count from habits h where user_id = ${userId} ORDER BY h.id desc limit ? offset ?`;
+    db.all(sql, [limit, offset], (err, rows) => {
       if (err) {
-        res.status(500).send("Internal Server Error");
-      } else {
-        res.render("home", { habits: rows });
+        res.status(500).send(`Internal Server Error\n${err}`);
       }
+      db.get(`select count(1) as count from habits`, (err, row) => {
+        if (err) {
+          res.status(500).send(`Internal Server Error\n${err}`);
+        } else {
+          const total = row.count;
+          const totalPage = Math.ceil(total / limit);
+          res.render("home", {
+            habits: rows,
+            currentPage: page,
+            totalPage: totalPage,
+          });
+        }
+      });
     });
+    // let sql = `
+    //   SELECT h.id as id, h.habit_name as habit_name, h.start_date as start_date, h.end_date as end_date, COUNT(r.habit_id) AS record_count FROM habits h LEFT JOIN records r ON h.id = r.habit_id WHERE h.user_id = ${userId} GROUP BY h.id, h.habit_name,h.start_date, h.end_date ORDER BY h.id desc
+    // `;
+    // let sql2 = `select id, habit_name, start_date, end_date, (select count(1) from records r where r.habit_id=h.id) count from habits h where user_id=${userId} ORDER BY h.id desc`;
+
+    // db.all(sql2, [], (err, rows) => {
+    //   if (err) {
+    //     res.status(500).send("Internal Server Error");
+    //   } else {
+    //     res.render("home", { habits: rows });
+    //   }
+    // });
   } else {
     res.redirect("/login");
   }
@@ -107,7 +129,7 @@ app.post("/login", (req, res) => {
       res.status(500).send(`Internal Server Error`);
     }
     console.log(row.count);
-    if (row != undefined) {
+    if (row.length > 0) {
       console.log(row);
       req.session.user = {
         id: row[0]["id"],
@@ -183,7 +205,29 @@ app.post("/habit/add", (req, res) => {
   });
   res.redirect("/");
 });
-
+app.get("/habit/edit/:id", (req, res) => {
+  const habitId = req.params.id;
+  const sql = `select habit_name, start_date, end_date from habits where id = ?`;
+  db.all(sql, [habitId], (err, row) => {
+    if (err) {
+      res.status(500).send(`Internal Server Error\n${err}`);
+    }
+    res.render("edithabit", { habit: row[0] });
+  });
+});
+app.post("/habit/edit/:id", (req, res) => {
+  const habitId = req.params.id;
+  let sql = `update habits 
+  set habit_name = '${req.body.habit_name}', start_date = '${req.body.start_date}', 
+  end_date = '${req.body.end_date}'
+  where id=${habitId}`;
+  db.run(sql, (err) => {
+    if (err) {
+      res.status(500).send("Internal Server Error");
+    }
+    res.redirect("/");
+  });
+});
 app.get("/habit/:id/record", (req, res) => {
   const habitId = req.params.id;
   console.log(habitId);
@@ -192,17 +236,33 @@ app.get("/habit/:id/record", (req, res) => {
     from records r inner join habits h
     where r.habit_id = h.id and r.habit_id=${habitId} order by 1 desc
   `;
-
-  console.log(sql);
-  db.all(sql, [], (err, rows) => {
+  // let sql = `
+  //   select row_number() over(order by id) as rn,
+  //   r.id as id, r.memo as memo, r.createdAt as createdAt, h.habit_name as habit_name
+  //   from records r inner join habits h
+  //   where r.habit_id = h.id and r.habit_id=${habitId} order by 1 desc
+  // `;
+  const record_sql = `
+    select row_number() over(order by id) as rn, id, memo, createdAt from records where habit_id = ?
+  `;
+  db.all(record_sql, [habitId], (err, rows) => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      console.log(rows);
-      res.render("record", { records: rows, habit: habitId });
+      res.status(500).send(`Internal Server Error\n${err}`);
     }
+    res.render("record", {
+      habit_id: habitId,
+      records: rows,
+    });
   });
+  // db.all(sql, [], (err, rows) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.status(500).send("Internal Server Error");
+  //   } else {
+  //     console.log(rows);
+  //     res.render("record", { records: rows, habit: habitId });
+  //   }
+  // });
 });
 
 app.get("/habit/:id/record/add", (req, res) => {
@@ -218,15 +278,38 @@ app.post("/habit/:id/record/add", (req, res) => {
   db.run(sql);
   res.redirect(`/habit/${habitId}/record`);
 });
-app.get("/habit/:id/record/remove/:rid", (req, res) => {
-  const id = req.params.id;
+app.get("/habit/:hid/record/:rid/remove", (req, res) => {
+  const hid = req.params.hid;
   const rid = req.params.rid;
   let sql = `
     delete from records where id = ${rid}
   `;
   db.run(sql);
-  res.redirect(`/habit/${id}/record`);
+  res.redirect(`/habit/${rid}/record`);
 });
+app.get("/habit/:hid/record/:rid/edit", (req, res) => {
+  const hid = req.params.hid;
+  const rid = req.params.rid;
+  let sql = `select memo from records where id = ?`;
+  db.all(sql, [rid], (err, row) => {
+    if (err) {
+      res.status(500).send(`Internal Server Error\n${err}`);
+    }
+    res.render("editrecord", { record: row[0] });
+  });
+});
+app.post("/habit/:hid/record/:rid/edit", (req, res) => {
+  const { hid, rid } = req.params;
+  let sql = `update records set memo = '${req.body.memo}' where id=${rid}`;
+  console.log(sql);
+  db.run(sql, (err) => {
+    if (err) {
+      res.status(500).send(`Internal Server Error : ${err}`);
+    }
+    res.redirect(`/habit/${hid}/record`);
+  });
+});
+app.get("/habit/:hid/record/:rid/remove");
 app.listen(PORT, () => {
   console.log(`${PORT}에서 습관 관리 서버 작동중`);
   console.log("http://localhost:3000/");
